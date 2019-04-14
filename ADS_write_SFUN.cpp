e0b486c6be731fecc8f25728d9247e0f0765a22f
Martin Dodek <dodekm1@gmail.com>
2019-04-14 09:34:36 +0200

    s fun

--- ADS_write_SFUN.cpp
+++ ADS_write_SFUN.cpp
@@ -14,13 +14,10 @@
 // its associated macro definitions.
 #include "simstruc.h"
 
-#define IS_PARAM_DOUBLE(pVal) (mxIsNumeric(pVal) && !mxIsLogical(pVal) &&\
-!mxIsEmpty(pVal) && !mxIsSparse(pVal) && !mxIsComplex(pVal) && mxIsDouble(pVal))
-
 AmsAddr   Addr;
 PAmsAddr  pAddr = &Addr;
 ADS_variable var;
-   
+
 
 // Function: mdlInitializeSizes ===============================================
 // Abstract:
@@ -28,33 +25,38 @@ ADS_variable var;
 //    block's characteristics (number of inputs, outputs, states, etc.).
 static void mdlInitializeSizes(SimStruct *S)
 {
-    // No expected parameters
-    ssSetNumSFcnParams(S, 0);
-
-    // Parameter mismatch will be reported by Simulink
+    ssSetNumSFcnParams(S, 0);  /* Number of expected parameters */
     if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
+        /* Return if number of expected != number of actual parameters */
         return;
     }
-
-    // Specify I/O
+    
+    ssSetNumContStates(S, 0);
+    ssSetNumDiscStates(S, 0);
+    
     if (!ssSetNumInputPorts(S, 1)) return;
-    ssSetInputPortWidth(S, 0, DYNAMICALLY_SIZED);
+    ssSetInputPortWidth(S, 0, 1);
+    ssSetInputPortRequiredContiguous(S, 0, true); /*direct input signal access*/
+    /*
+     * Set direct feedthrough flag (1=yes, 0=no).
+     * A port has direct feedthrough if the input is used in either
+     * the mdlOutputs or mdlGetTimeOfNextVarHit functions.
+     */
     ssSetInputPortDirectFeedThrough(S, 0, 1);
-    if (!ssSetNumOutputPorts(S,1)) return;
-    ssSetOutputPortWidth(S, 0, DYNAMICALLY_SIZED);
-
+    
+    if (!ssSetNumOutputPorts(S, 0)) return;
+    
     ssSetNumSampleTimes(S, 1);
-
-    // Reserve place for C++ object
-    ssSetNumPWork(S, 1);
-
-    ssSetSimStateCompliance(S, USE_CUSTOM_SIM_STATE);
-
-    ssSetOptions(S,
-                 SS_OPTION_WORKS_WITH_CODE_REUSE |
-                 SS_OPTION_EXCEPTION_FREE_CODE |
-                 SS_OPTION_DISALLOW_CONSTANT_SAMPLE_TIME);
-
+    ssSetNumRWork(S, 0);
+    ssSetNumIWork(S, 0);
+    ssSetNumPWork(S, 0);
+    ssSetNumModes(S, 0);
+    ssSetNumNonsampledZCs(S, 0);
+    
+    /* Specify the sim state compliance to be same as a built-in block */
+    ssSetSimStateCompliance(S, USE_DEFAULT_SIM_STATE);
+    ssSetOptions(S, 0);
+    
 }
 
 
@@ -67,7 +69,14 @@ static void mdlInitializeSampleTimes(SimStruct *S)
 {
     ssSetSampleTime(S, 0, INHERITED_SAMPLE_TIME);
     ssSetOffsetTime(S, 0, 0.0);
-    ssSetModelReferenceSampleTimeDefaultInheritance(S); 
+    ssSetModelReferenceSampleTimeDefaultInheritance(S);
+}
+
+
+static void mdlInitializeConditions(SimStruct *S)
+{
+    
+    
 }
 
 // Function: mdlStart =======================================================
@@ -75,30 +84,43 @@ static void mdlInitializeSampleTimes(SimStruct *S)
 //   This function is called once at start of model execution. If you
 //   have states that should be initialized once, this is the place
 //   to do it.
-#define MDL_START
+
 static void mdlStart(SimStruct *S)
 {
     
     ADS_init(pAddr, 0, ADS_create_ip(10, 3, 1, 138, 3, 1), AMSPORT_R0_PLC_TC3);
-    ADS_init_var(&var,"GVL.spirala",TC_INT_type);	
-   
+    ADS_init_var(&var,"GVL.spirala",TC_INT_type);
+    
 }
 
 // Function: mdlOutputs =======================================================
 // Abstract:
 //   In this function, you compute the outputs of your S-function
 //   block.
-static void mdlOutputs(SimStruct *S, int_T tid)
+static void mdlOutputs(SimStruct *S,int_T tid)
 {
-   
-    // Retrieve C++ object from the pointers vector
- 
-             
-             
+    
+    
 }
 
-/* Define to indicate that this S-Function has the mdlG[S]etSimState methods */
-#define MDL_SIM_STATE
+static void mdlUpdate(SimStruct *S,int_T tid)
+{
+    const real_T *u = ssGetInputPortRealSignal(S,0);
+    ADS_var_value_set_double(&var,*u);
+    ADS_variable_write(pAddr, &var);
+    
+}
+
+/* Function: mdlDerivatives =================================================
+ * Abstract:
+ *    In this function, you compute the S-function block's derivatives.
+ *    The derivatives are placed in the derivative vector, ssGetdX(S).
+ */
+static void mdlDerivatives(SimStruct *S)
+{
+    
+}
+
 
 /* Function: mdlGetSimState =====================================================
  * Abstract:
@@ -106,11 +128,9 @@ static void mdlOutputs(SimStruct *S, int_T tid)
  */
 static mxArray* mdlGetSimState(SimStruct* S)
 {
-   
-    const real_T *u = ssGetInputPortRealSignal(S,0);
-   ADS_var_value_set_double(&var,10);
-   ADS_variable_write(pAddr, &var);
-        
+    
+    
+    
 //Retrieve C++ object from the pointers vector
     return mxCreateDoubleScalar(0);
 }
@@ -120,10 +140,10 @@ static mxArray* mdlGetSimState(SimStruct* S)
  */
 static void mdlSetSimState(SimStruct* S, const mxArray* ma)
 {
-   
+    
 // Retrieve C++ object from the pointers vector
-   // DoubleAdder *da = static_cast<DoubleAdder*>(ssGetPWork(S)[0]);
-   // da->SetPeak(mxGetPr(ma)[0]);
+    // DoubleAdder *da = static_cast<DoubleAdder*>(ssGetPWork(S)[0]);
+    // da->SetPeak(mxGetPr(ma)[0]);
 }
 
 // Function: mdlTerminate =====================================================
@@ -133,9 +153,9 @@ static void mdlSetSimState(SimStruct* S, const mxArray* ma)
 //   allocated in mdlStart, this is the place to free it.
 static void mdlTerminate(SimStruct *S)
 {
-    // Retrieve and destroy C++ object
-     ADS_release_handler(pAddr, &var);
-     ADS_deinit();
+    
+    ADS_release_handler(pAddr, &var);
+    ADS_deinit();
 }
 
 // Required S-function trailer
--- build_script.m
+++ build_script.m
@@ -8,5 +8,5 @@ mex ADS_write_mex.cpp ADS_lib.cpp -LC:\Users\Admin\Desktop\ADS_beckhoff_matlab-m
 mex ADS_read_SFUN.cpp ADS_lib.cpp -LC:\Users\Admin\Desktop\ADS_beckhoff_matlab-master -lTcAdsDll
 mex ADS_write_SFUN.cpp ADS_lib.cpp -LC:\Users\Admin\Desktop\ADS_beckhoff_matlab-master -lTcAdsDll
 %%
-%ADS_write_mex([10, 3, 1, 138, 3, 1],'GVL.tmp',TC_INT_type,0.5)
-%A=ADS_read_mex([10, 3, 1, 138, 3, 1],'GVL.tmp',TC_INT_type);
+mex ADS_read_SFUN.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
+mex ADS_write_SFUN.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
