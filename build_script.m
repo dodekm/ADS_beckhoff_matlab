9b95a1cdba2f4fd5a57857f5f7d31ae36029fd99
Martin Dodek <dodekm1@gmail.com>
2019-04-24 16:56:05 +0200

    updated build script

--- build_script.m
+++ build_script.m
@@ -1,13 +1,9 @@
-
 %skript na kompilaciu MEX funkcii a S funkcii
-%je nutne rucne zmenit linkovaci adresar na aktualny pracovny prieconok
-pwd
-mex ADS_read_mex.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
-mex ADS_write_mex.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
-mex ADS_open_mex.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
-mex ADS_close_mex.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
 
-mex ADS_read_SFUN.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
-mex ADS_write_SFUN.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
-mex ADS_setup_SFUN.cpp ADS_lib.cpp -Lx:\Dropbox\FEI\OP\ADS_beckhoff_matlab -lTcAdsDll
-
+mex ('ADS_write_mex.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
+mex ('ADS_read_mex.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
+mex ('ADS_open_mex.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
+mex ('ADS_close_mex.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
+mex ('ADS_setup_SFUN.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
+mex ('ADS_write_SFUN.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
+mex ('ADS_read_SFUN.cpp','ADS_lib.cpp',strcat('-L',pwd()),'-lTcAdsDll')
